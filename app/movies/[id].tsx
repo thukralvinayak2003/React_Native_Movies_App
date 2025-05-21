e19da7a19139d5cb00ce4bab68b531c68329fdc3
thukralvinayak2003 <thukralvinayak2003@gmail.com>
2025-05-21 08:49:40 +0530

    first commit

---
+++ app/movies/[id].tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+
+const MovieDetails = () => {
+  return (
+    <View>
+      <Text>MovieDetails</Text>
+    </View>
+  )
+}
+
+export default MovieDetails
+
+const styles = StyleSheet.create({})
